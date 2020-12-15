9fd7301a3fc20d15caeffbebb6b992601feba0c1
BW <>
2020-12-15 16:54:27 +0800

    整理mysql table架構 依照table調整api

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.urlencoded({
 }));
 //而JavaScript中，所有的插件都是一個物件(沒有類別的概念，一導入就是實體，需要用變數接住)
 const mysql = require('mysql'); //Import mysql套件
+const {
+    json
+} = require('body-parser');
 const port = process.env.PORT || 3000; //const是區域變數，不可改(類似java final)
 //當專案產出、發佈到線上公開存取，這個數值會隨著不同的主機環境動態改變。所以這裡必須要設定一個動態的數值。
 app.listen(port, () => {
@@ -51,7 +54,6 @@ app.get('/api', (req, res, next) => {
 
 //創一個命名為api2的路徑，開放接收get request的API
 app.get('/api2', (req, res, next) => {
-
     res.json('testtest'); //用json格式response 'testtest'
 });
 
@@ -66,87 +68,63 @@ app.post('/api3', jsonParser, (req, res) => {
     res.json(product);
 });
 
-app.get('/getQuotaC', (req, res) => {
-    conn.query('SELECT (quota) FROM quota WHERE who = "Christina" LIMIT 1;', function(err, result, fields) {
+app.post('/getQuota', (req, res) => {
+    let user = req.body.user
+    conn.query('SELECT quota FROM users WHERE name = "' + user + '" LIMIT 1;', function(err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    })
+});
+
+app.post('/getTickets', (req, res) => {
+    let user = req.body.user
+    let gotQuota
+    conn.query('SELECT quota FROM users WHERE name = "' + user + '" LIMIT 1;', function(err, result, fields) {
+        if (err) throw err;
+        gotQuota = result[0].quota
+
+        conn.query('SELECT * FROM tickets WHERE user_id IN (SELECT id from users WHERE name = "' + user + '") ORDER BY create_at;', function(err, result, fields) {
             if (err) throw err;
-            console.log(result);
-            res.json(result);
+            let gotTickets = [];
+            for (let i = 0; i < result.length; i++) {
+                let arr = [];
+                arr = {
+                    id: result[i].id,
+                    content: result[i].content,
+                    create_at: result[i].create_at,
+                    checked: result[i].checked,
+                    deleted: result[i].deleted
+                }
+                gotTickets.push(arr);
+            }
+            let data = {
+                quota: gotQuota,
+                tickets: gotTickets
+            }
+            console.log(data);
+            res.json(data);
         })
-        // res.json(result);
-
-    // });
-});
-
-app.get('/getQuotaB', (req, res) => {
-    conn.query('SELECT (quota) FROM quota WHERE who = "Billy" LIMIT 1;', function(err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            res.json(result);
-        })
-        // res.json(result);
-
-    // });
-});
-
-app.get('/getTicketsC', (req, res) => {
-    conn.query('SELECT * FROM tickets_C ORDER BY create_at;', function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
     })
-});
 
-
-app.get('/getTicketsB', (req, res) => {
-    conn.query('SELECT * FROM tickets_B ORDER BY create_at;', function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-    })
-});
-
-app.get('/getTestA', (req, res) => {
-    conn.query('SELECT * FROM testA ORDER BY create_at;', function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-    })
-});
-
-app.get('/getTestA', (req, res) => {
-    conn.query('SELECT * FROM testA ORDER BY create_at;', function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-    })
-});
-
-app.get('/getTestB', (req, res) => {
-    conn.query('SELECT * FROM testB ORDER BY create_at;', function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-    })
 });
 
 app.post('/postNewTicket', jsonParser, (req, res) => {
-    let name = req.body.ticketName;
-    console.log("new ticket:", name);
-    conn.query(`INSERT INTO tickets (content) VALUES ("` + name + `") ;`, function(err, result, fields) {
+    let ticketName = req.body.ticketName;
+    let user = req.body.user
+    console.log("new ticket:", ticketName);
+    conn.query(`INSERT INTO tickets (user_id, content) VALUES ((SELECT id FROM users WHERE name = "` + user + `") , "` + ticketName + `");`, function(err, result, fields) {
         if (err) throw err;
-    })
-
-    conn.query(`SELECT * FROM tickets WHERE content = "` + name + `" LIMIT 1;`, function(err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
+        conn.query(`SELECT * FROM tickets WHERE id = (SELECT LAST_INSERT_ID());`, function(err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            res.json(result);
+        })
     })
 });
 
-
 app.post('/checkTicket', jsonParser, (req, res) => {
-    // res.status(404).send("hello??? 404");
-    var id = req.body.id; //var是全域變數
+    let id = req.body.id; //var是全域變數
     console.log("test", id)
     conn.query(`UPDATE tickets SET checked = true WHERE id = ` + id, function(err, result, fields) {
         if (err) throw err;
@@ -154,7 +132,7 @@ app.post('/checkTicket', jsonParser, (req, res) => {
             if (err) throw err;
             console.log(result);
             res.json(result);
-        });
+        })
     })
 });
 
@@ -166,7 +144,7 @@ app.post('/deleteTicket', jsonParser, (req, res) => {
             if (err) throw err;
             console.log(result);
             res.json(result);
-        });
+        })
     })
 });
 
@@ -185,3 +163,23 @@ app.get('/getBirthdayLetter', (req, res) => {
         res.json(result);
     })
 });
+
+app.get('/renewAutoIncrement', jsonParser, (req, res) => {
+    conn.query(`SELECT * FROM tickets;`, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        for (let i = 0; i < result.length; i++) {
+            conn.query(`SELECT * FROM tickets LIMIT 1 OFFSET ${i};`, (err, result1) => {
+                if (err) {
+                    throw err;
+                }
+                conn.query(`UPDATE tickets SET id = ${i} WHERE id = ${result[i].id};`, (err, result2) => {
+                    if (err) {
+                        throw err;
+                    }
+                })
+            })
+        }
+    })
+})
